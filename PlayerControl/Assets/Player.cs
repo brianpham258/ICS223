f37c2dbd0b7c47f5cc1c1276327f9457be144045
Quang Pham <quanghuy25899@gmail.com>
2020-01-30 11:41:19 -0800

    Player control

---
+++ PlayerControl/Assets/Player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private float speed = 9.0f;
+    private float rotationSpeed = 200.0f;
+    private CharacterController charController;
+    // Start is called before the first frame update
+    void Start()
+    {
+        charController = GetComponent<CharacterController>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X");
+        Vector3 rotation = new Vector3(0, mouseX, 0) * rotationSpeed * Time.deltaTime;
+        transform.Rotate(rotation);
+
+        float deltaX = Input.GetAxis("Horizontal");
+        float deltaY = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(deltaX, 0, deltaY) * speed * Time.deltaTime;
+        movement = transform.TransformDirection(movement);
+        charController.Move(movement);
+        //transform.Translate(movement);
+    }
+}
